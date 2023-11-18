fc64a4e67f4f6650a673cf55d79de917cfb12278
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2023-11-19 02:27:09 +0700

    Update product.mk

--- product.mk
+++ product.mk
@@ -1,1 +1,6 @@
 echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := android-certs/releasekey" > android-certs/product.mk
+
+ota_from_target_files -k android-certs/releasekey \
+    --block --backup=true \
+    signed-target_files.zip \
+    signed-ota_update.zip
